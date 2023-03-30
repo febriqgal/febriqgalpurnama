6a747f5fddf75a36a4a4b0b6b304aed640fc4cd2
Febriqgal Purnama <febriqgalp@gmail.com>
2023-03-30 14:31:57 +0700

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
-
+import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import NextNProgress from "nextjs-progressbar";
+import { useTheme } from "next-themes";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { systemTheme, theme, setTheme } = useTheme();
+  return (
+    <>
+      <NextNProgress
+        height={3}
+        color="#0F172A"
+        options={{ showSpinner: false }}
+      />
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
---
+++ src/pages/project/index.js
@@ -0,0 +1,10 @@
+import Layout from "@/components/layout";
+import React from "react";
+
+export default function index() {
+  return (
+    <Layout>
+      <h1>Project</h1>
+    </Layout>
+  );
+}
