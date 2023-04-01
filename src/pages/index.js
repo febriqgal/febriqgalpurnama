c0ac333b89874f266765535fcf7a7bb91e008040
Febriqgal Purnama <febriqgalp@gmail.com>
2023-04-01 19:55:42 +0700

    daskdaskdaskdas

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,7 +56,7 @@ export default function Home() {
         </Modal.Footer>
       </Modal>
       <Image
-        className="absolute -z-50 top-0 left-0 min-h-screen"
+        className="absolute -z-50 top-0 left-0 right-0 bottom-0 min-h-screen w-full"
         src={bg}
         alt="#"
       />
