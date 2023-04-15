762a54a52475cc72ce9a71354760cc8cc8ee45de
Febriqgal Purnama <febriqgalp@gmail.com>
2023-04-15 22:29:46 +0700

    dasdasdasdas

--- src/components/headerC.js
+++ src/components/headerC.js
@@ -17,12 +17,20 @@ export default function HeaderC() {
       <div>
         <h1 className="text-lg font-medium">{`Hi,👋 I'm`}</h1>
         <h1 className={clsx("text-5xl font-black")}>Febriqgal Purnama</h1>
-        <TypeAnimation
-          sequence={["Web App Developer", 1000, "Mobile App Developer", 1000]}
-          speed={50}
-          style={{ fontSize: "16px" }}
-          repeat={Infinity}
-        />
+        <div className="flex gap-2">
+          <h1>{"a"}</h1>
+          <TypeAnimation
+            sequence={[
+              " Web App Developer",
+              1000,
+              " Mobile App Developer",
+              1000,
+            ]}
+            speed={50}
+            style={{ fontSize: "16px" }}
+            repeat={Infinity}
+          />
+        </div>
         <TechstackC />
         <div className="flex lg:flex-row flex-col gap-2 mt-4 text-center">
           <Link
