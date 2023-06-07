9d5f8c5ab5212aa56817532d8f539055622e1dbe
Febriqgal Purnama <febriqgalp@gmail.com>
2023-06-07 15:13:20 +0700

    dasdasdasdasdas

--- src/components/layout/navC.js
+++ src/components/layout/navC.js
@@ -4,11 +4,12 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { MoonIcon } from "../icon/moonicon";
 import { SunIcon } from "../icon/sunicon";
+import Image from "next/image";
+import Logo from "../../../public/logo transparant.png";
 export default function NavC() {
   const { systemTheme, theme, setTheme } = useTheme();
 
   const navigation = [
-    { title: "Home", href: "/" },
     { title: "Project", href: "/project" },
     { title: "Portfolio", href: "/portfolio" },
     { title: "Certification", href: "/certification" },
@@ -17,10 +18,17 @@ export default function NavC() {
   const route = useRouter();
   return (
     <div>
-      
       {/* <div className="h-4 w-full bg-gradient-to-r from-slate-400 via-orange-300 to-yellow-200 border-b-2 decoration-white border-dotted border-slate-950 dark:border-slate-50"></div> */}
       <div className="bg-slate-50 dark:bg-transparent bg-transparent text-sm lg:text-base px-5 lg:px-36 dark:bg-slate-950 flex justify-between items-center py-7 justify-items-center  w-full ">
         <div className="flex items-center lg:justify-center gap-4 place-items-center font-bold flex-wrap">
+          <Link href={'/'}>
+            <Image
+              title="Beranda Febriqgal Purnama, S.Kom."
+              width={60}
+              src={Logo}
+              alt="FP"
+            />
+          </Link>
           {navigation.map((e, i) => {
             return (
               <Link
