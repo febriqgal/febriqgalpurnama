8da30839732c136e0ed880076a1c32000d9f3d8f
unknown <febriqgalp@gmail.com>
2023-07-06 08:57:36 +0700

    dasdasdas

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,15 @@ import Layout from "@/components/layout";
 import PortfolioC from "@/components/portfolioC";
 import Image from "next/image";
 import bg from "../../public/20.png";
+import { Tabs, Tab, Card, CardBody } from "@nextui-org/react";
+
 export default function Home() {
   return (
     <Layout>
       <HeaderC />
       <CertificationC />
       <PortfolioC />
+      <h1 id="ww">sss</h1>
     </Layout>
   );
 }
